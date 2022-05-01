ce4a7c87d2bc0d3fdccb48e1ffcc741723cc5c19
Beasters <1229435236@qq.com>
2022-05-01 15:52:11 +0800

    update server pool

--- src/main/java/io/github/beastars1/ouroboros/loadbalance/ServerPool.java
+++ src/main/java/io/github/beastars1/ouroboros/loadbalance/ServerPool.java
@@ -2,22 +2,24 @@ package io.github.beastars1.ouroboros.loadbalance;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class ServerPool {
     private final List<Server> servers = new ArrayList<>();
-    private final AtomicInteger atomic = new AtomicInteger();
+    private int i = 0;
 
     public ServerPool(List<Server> servers) {
+        if (servers.size() == 0) {
+            throw new RuntimeException("servers size should be greater than 0");
+        }
         this.servers.addAll(servers);
     }
 
     public Server getServer() {
-        int i = atomic.getAndIncrement();
-        Server server = servers.get(i);
-        if (atomic.get() >= servers.size()) {
-            atomic.set(0);
+        if (i >= servers.size()) {
+            i = 0;
         }
+        Server server = servers.get(i);
+        i++;
         return server;
     }
 }
